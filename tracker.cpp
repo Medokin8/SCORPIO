80b2267833057764094579c135a0ab9678fdce04
Medokin8 <81477574+Medokin8@users.noreply.github.com>
2023-03-18 01:51:47 +0100

    Add files via upload

--- tracker.cpp
+++ tracker.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
+#include <string>
+
 
 using namespace std;
 
+struct cpu_stat {
+    unsigned int user;
+    unsigned int nice;
+    unsigned int system;
+    unsigned int idle;
+    unsigned int iowait;
+    unsigned int irq;
+    unsigned int softirq;
+};
+
+
+
 
 int main(){
-    ofstream myfile;
-    myfile.open ("system_data_readings.txt");
-    myfile << "Writing this to a file.\n";
-    myfile.close();
-    return 0;
+
+    //Reading CPU
+    while(1)
+    {   
+        string data_stat;
+        
+        ifstream reading_data;
+        reading_data.open("/proc/stat");
+
+        //skip all usage of CPU
+        getline(reading_data, data_stat);
+
+        struct cpu_stat cpu_sta_0, cpu_stat_1;
+        
+        sleep(1);
+
+
+        
+
+        //Save to file
+        ofstream saving_data;
+        saving_data.open ("system_data_readings.txt");
+        saving_data << "Writing this to a file.\n";
+        saving_data.close();
+        sleep(5);
+    }
 }
