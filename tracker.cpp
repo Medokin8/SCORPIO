fd8afa4abae36332a6647983495b6dc4e390943f
Medokin8 <81477574+Medokin8@users.noreply.github.com>
2023-04-06 14:15:12 +0200

    Add files via upload
    finished functions:
    Reading_CPU, 
    Reading_TEMP

--- tracker.cpp
+++ tracker.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 #include <unistd.h>
 #include <string>
+#include <sstream>
+#include <vector>
 
 
 using namespace std;
 
-struct cpu_stat {
+struct cpu_stat{
+    string core;
     unsigned int user;
     unsigned int nice;
     unsigned int system;
@@ -14,36 +17,126 @@ struct cpu_stat {
     unsigned int iowait;
     unsigned int irq;
     unsigned int softirq;
+    unsigned int steal;
+    unsigned int guest;
+    unsigned int guest_nice;
 };
 
+struct temp_stat{
+
+};
+
+void Reading_CPU(ofstream & saving_data)
+{
+    string data;
+    cpu_stat cpu;
+
+    ifstream reading_data;
+    reading_data.open("/proc/stat");
+    
+    // ofstream saving_data;
+    // saving_data.open ("system_data_readings.txt");
+
+    getline(reading_data, data);
+
+    stringstream iss(data);
+    string cpu_label;
+    iss >> cpu_label;
+
+    cpu_label = cpu_label + " ";
+    while(cpu_label != "intr")
+    {
+        iss >> cpu.user;
+        iss >> cpu.nice;
+        iss >> cpu.system;
+        iss >> cpu.idle;
+        iss >> cpu.iowait;
+        iss >> cpu.irq;
+        iss >> cpu.softirq;
+        iss >> cpu.steal;
+        iss >> cpu.guest;
+        iss >> cpu.guest_nice;
+        
+        long long total_time = cpu.user + cpu.nice + cpu.system + cpu.idle + cpu.iowait + cpu.irq + cpu.softirq;
+        //cout << "total_time:    " << total_time << endl;
+
+        long long total_idle = cpu.idle - cpu.iowait;
+        //cout << "total_idle:    " << total_idle << endl;
+
+        double load_core = 100.0 * (total_time - total_idle) / total_time;
+        //cout << cpu_label << ":          " << load_core <<" %" << endl;
+        //cout << "diff:          " << (total_time - total_idle) << endl;
+
+        //cout << cpu_label;
+        saving_data << cpu_label;
+        saving_data << "         " << load_core <<" % \n";
+
+        //cout<<endl;
+
+        getline(reading_data, data);
+
+        stringstream iss(data);
+        iss >> cpu_label;
+    }
+    
+    reading_data.close();
+}
 
 
+void Reading_TEMP(ofstream & saving_data)
+{
+    string data;
+    double numeric_data;
+
+    ifstream reading_data;
+    //string directory = "/sys/class/thermal/cooling_device";
+    string directory = "/sys/class/thermal/thermal_zone";
+    int i = 0;
+
+    //string num_dir = directory + to_string(i) + "/type";
+    string num_dir = directory + to_string(i) + "/temp";
+    
+    reading_data.open(num_dir);
+
+    while(reading_data.is_open())
+    {
+        getline(reading_data, data);
+        numeric_data = stod(data);
+
+        //cout << num_dir << "   " << data << endl;
+
+        string core = "Core" + to_string(i);
+        saving_data << core ;
+        //saving_data << "        " <<data <<" C \n";
+        saving_data << "        " << numeric_data/1000.0 <<" C \n";
+
+        reading_data.close();
+
+        i++;
+
+        //num_dir = directory + to_string(i) + "/type";
+        num_dir = directory + to_string(i) + "/temp";
+
+        reading_data.open(num_dir);
+    }
+    
+}
+
+void Reading_RAM(ofstream & saving_data)
+{
+
+}
 
 int main(){
 
-    //Reading CPU
-    while(1)
-    {   
-        string data_stat;
-        
-        ifstream reading_data;
-        reading_data.open("/proc/stat");
+    ofstream saving_data;
+    saving_data.open ("system_data_readings.txt");
 
-        //skip all usage of CPU
-        getline(reading_data, data_stat);
+    Reading_CPU(saving_data);
+    Reading_TEMP(saving_data);
+    Reading_RAM(saving_data);
 
-        struct cpu_stat cpu_sta_0, cpu_stat_1;
-        
-        sleep(1);
+    saving_data.close();
 
-
-        
-
-        //Save to file
-        ofstream saving_data;
-        saving_data.open ("system_data_readings.txt");
-        saving_data << "Writing this to a file.\n";
-        saving_data.close();
-        sleep(5);
-    }
+    return 0;
 }
